51e34e57ad4c794ed518eba4a15ab81af5b59b8d
mohak92 <tamhane.mohak@gmail.com>
2021-01-02 21:23:20 -0500

    app component update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "./translate.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'translator';
+  text: string;
+  translatedText: string;
+
+  constructor(private translateService: TranslateService) {}
+
+  submit() {
+    this.translateService.translate(this.text).subscribe((result) => {
+      this.translatedText = result;
+    });
+  }
 }
