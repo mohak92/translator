84367bef0377fa33172697f5c2095a7f35f8588f
mohak92 <tamhane.mohak@gmail.com>
2021-01-03 01:12:21 -0500

    fixes

--- src/app/translate.service.ts
+++ src/app/translate.service.ts
@@ -3,21 +3,37 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TranslateService {
-  url = 'https://translation.googleapis.com/language/translate/v2?key=';
+  url =
+    'https://google-translate1.p.rapidapi.com/language/translate/v2';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   translate(text: string) {
-    return this.http.post(this.url, {
-      "q": text,
-      "target": "es"
-    }).pipe(
-      map((res: any) => {
-        return res.data.translations[0].translatedText;
-      })
-    );
+    var data = new URLSearchParams({
+      q: text,
+      target: 'es'
+    });
+    return this.http
+      .post(
+        this.url,
+        data.toString(),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept-Encoding': 'application/gzip',
+            'X-Rapidapi-Key': '',
+            'X-Rapidapi-Host': 'google-translate1.p.rapidapi.com',
+            'Host': 'google-translate1.p.rapidapi.com'
+          },
+        }
+      )
+      .pipe(
+        map((res: any) => {
+          return res.data.translations[0].translatedText;
+        })
+      );
   }
 }
